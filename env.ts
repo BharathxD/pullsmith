c05a23e474620385b78924af5dc84b1790f1b570
Bharath Lakshman Kumar <bharathxxd@gmail.com>
2025-07-04 23:24:10 +0530

    feat: add Upstash Redis environment variables and dependencies for Redis integration

--- env.ts
+++ env.ts
@@ -9,6 +9,9 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string().min(1),
     GITHUB_APP_ID: z.string().min(1),
     GITHUB_APP_PRIVATE_KEY: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
+    UPSTASH_REDIS_URL: z.string().url(),
   },
 
   /**
